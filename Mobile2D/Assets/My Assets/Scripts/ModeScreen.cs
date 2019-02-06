813cc64de34d149c755c472c87d65d68c89fda84
IbarraranSoftware <46429550+IbarraranSoftware@users.noreply.github.com>
2019-02-05 20:57:39 -0600

    2.1

--- Mobile2D/Assets/My Assets/Scripts/ModeScreen.cs
+++ Mobile2D/Assets/My Assets/Scripts/ModeScreen.cs
@@ -8,22 +8,17 @@ public class ModeScreen : MonoBehaviour
     public Text mode;
     public Text classic;
     public Text color;
-    public Text tournament;
     public Image classicBox;
     public Image colorBox;
-    public Image tournamentBox;
     public Image classicBoxFill;
     public Image colorBoxFill;
-    public Image tournamentBoxFill;
     public RectTransform classicButton;
     public RectTransform colorButton;
-    public RectTransform tournamentButton;
 
     //This is the aspect of ObjectPosition/ScreenHeight in a correct looking screen
     private float modePos = 0.3130f;
     private float classicPos = 0.0521f;
-    private float colorPos = -0.1042f;
-    private float tournamentPos = -0.2604f;
+    private float colorPos = -0.2083f;
 
     //This is the aspect of ContainerHeight/ScreenHeight in a correct looking screen
     private float boxHeight = 0.1042f;
@@ -44,28 +39,22 @@ public class ModeScreen : MonoBehaviour
         mode.rectTransform.position = new Vector2(Screen.width/2, (Screen.height/2)+(Screen.height*modePos));
         classicButton.position = new Vector2(Screen.width/2, (Screen.height/2)+(Screen.height*classicPos));
         colorButton.position = new Vector2(Screen.width/2, (Screen.height/2)+(Screen.height*colorPos));
-        tournamentButton.position = new Vector2(Screen.width/2, (Screen.height/2)+(Screen.height*tournamentPos));
 
         //Sets the correct size for every screen size
         mode.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height*modeSize);
         classic.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height*boxHeight);
         color.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height*boxHeight);
-        tournament.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height * boxHeight);
         classicBox.rectTransform.sizeDelta = new Vector2(Screen.width*boxWidth, Screen.height*boxHeight);
         colorBox.rectTransform.sizeDelta = new Vector2(Screen.width * boxWidth, Screen.height * boxHeight);
-        tournamentBox.rectTransform.sizeDelta = new Vector2(Screen.width * boxWidth, Screen.height * boxHeight);
         classicBoxFill.rectTransform.sizeDelta = new Vector2(Screen.width*boxFillWidth, Screen.height*boxFillHeight);
         colorBoxFill.rectTransform.sizeDelta = new Vector2(Screen.width * boxFillWidth, Screen.height * boxFillHeight);
-        tournamentBoxFill.rectTransform.sizeDelta = new Vector2(Screen.width * boxFillWidth, Screen.height * boxFillHeight);
         classicButton.sizeDelta = new Vector2(Screen.width * boxWidth, Screen.height * boxHeight);
         colorButton.sizeDelta = new Vector2(Screen.width * boxWidth, Screen.height * boxHeight);
-        tournamentButton.sizeDelta = new Vector2(Screen.width * boxWidth, Screen.height * boxHeight);
 
         //Sets th fontsize in the correct aspect
         mode.fontSize = Mathf.RoundToInt(Screen.height*modeSize);
         classic.fontSize = Mathf.RoundToInt(Screen.height*textSize);
         color.fontSize = Mathf.RoundToInt(Screen.height*textSize);
-        tournament.fontSize = Mathf.RoundToInt(Screen.height * textSize)-10;
     }
 
     // Update is called once per frame
